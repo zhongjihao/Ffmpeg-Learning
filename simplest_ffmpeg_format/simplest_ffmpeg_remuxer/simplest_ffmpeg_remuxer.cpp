ffe235c853c6d14492c6c8978fb498bacc18c897
zhongjihao <zhongjihao@aisafer.com>
2019-07-25 11:49:56 +0800

    简单的实现无编解码的视频格式转换的例子

--- simplest_ffmpeg_format/simplest_ffmpeg_remuxer/simplest_ffmpeg_remuxer.cpp
+++ simplest_ffmpeg_format/simplest_ffmpeg_remuxer/simplest_ffmpeg_remuxer.cpp
@@ -2,9 +2,7 @@
     > File Name: simplest_ffmpeg_remuxer.cpp
     > Author: zhongjihao
     > Mail: zhongjihao100@163.com 
-    > Created Time: 2018年03月16日 星期五 11时04分25秒
-	本程序实现了视频封装格式之间的转换。
-	需要注意的是本程序并不改变视音频的编码格式。
+    > 瑙嗛灏佽鏍煎紡杞崲,鏃犵紪瑙ｇ爜
  ************************************************************************/
 
 
@@ -18,7 +16,6 @@ extern "C"
 #endif
 
 #include <libavutil/log.h>
-#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 
 #ifdef __cplusplus
@@ -26,18 +23,18 @@ extern "C"
 #endif
 
 
-
 int main(int argc, char* argv[])
 {
 	char errors[1024];
 	AVOutputFormat *ofmt = NULL;
-	AVBitStreamFilterContext *vbsf = NULL;
 	//Input AVFormatContext and Output AVFormatContext
 	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
 	AVPacket pkt;
 	const char *in_filename, *out_filename;
 	int ret, i;
-	int frame_index = 0;
+	int stream_index = 0;
+	int *stream_mapping = NULL;
+    int stream_mapping_size = 0;
 	
 	av_log_set_level(AV_LOG_DEBUG);
 
@@ -52,7 +49,6 @@ int main(int argc, char* argv[])
 
 	av_register_all();
 
-	//打开输入文件，初始化输入视频码流的AVFormatContex
 	if((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
 		av_strerror(ret,errors,1024);
 		av_log(NULL,AV_LOG_ERROR,"Can't open input file: %s,%d(%s)\n",in_filename,ret,errors);
@@ -64,53 +60,70 @@ int main(int argc, char* argv[])
 		av_log(NULL,AV_LOG_ERROR,"Failed to retrieve input stream information: %d(%s)\n",ret,errors);
 		goto end;
 	}
-
 	av_dump_format(ifmt_ctx, 0, in_filename, 0);
 
-	vbsf = av_bitstream_filter_init("h264_mp4toannexb");
-	if(vbsf == NULL){
-		av_log(NULL,AV_LOG_ERROR,"bitstream filter is null\n");
-		goto end;
-	}
-    av_register_bitstream_filter(const_cast<AVBitStreamFilter*>(vbsf->filter));
-
-	//初始化输出视频码流的AVFormatContext
 	avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
 	if(!ofmt_ctx) {
 		av_log(NULL,AV_LOG_ERROR,"Could not create output context\n");
 		ret = AVERROR_UNKNOWN;
 		goto end;
 	}
+	stream_mapping_size = ifmt_ctx->nb_streams;
+    stream_mapping = (int*)av_mallocz_array(stream_mapping_size, sizeof(*stream_mapping));
+    if (!stream_mapping) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
 	ofmt = ofmt_ctx->oformat;
+    av_log(NULL,AV_LOG_DEBUG,"input file nb_streams: %d\n",ifmt_ctx->nb_streams);
 
 	for(i = 0; i < ifmt_ctx->nb_streams; i++) {
-		//根据输入流创建输出流(Create output AVStream according to input AVStream)
+		//Create output AVStream according to input AVStream
 		AVStream *in_stream = ifmt_ctx->streams[i];
-		AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+		AVCodecParameters *in_codecpar = in_stream->codecpar;
+		if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
+            stream_mapping[i] = -1;
+            continue;
+        }
+		stream_mapping[i] = stream_index++;
+
+		AVStream *out_stream = avformat_new_stream(ofmt_ctx, NULL);
 		if(!out_stream) {
             av_log(NULL,AV_LOG_ERROR,"Failed allocating output stream\n");
 			ret = AVERROR_UNKNOWN;
 			goto end;
 		}
-		//复制AVCodecContext的设置(Copy the settings of AVCodecContext)
-		ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
-		if(ret < 0){
-			av_strerror(ret,errors,1024);
-            av_log(NULL,AV_LOG_ERROR,"Failed to copy context from input to output stream codec context: %d(%s)\n",ret,errors);
-			goto end;
-		}
-		out_stream->codec->codec_tag = 0;
-		if(ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER) {
-            av_log(NULL,AV_LOG_DEBUG,"oformat->flags: %d\n",ofmt_ctx->oformat->flags);
-			out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-		}
+
+        //Copy the settings of AVCodecParameters
+		ret = avcodec_parameters_copy(out_stream->codecpar, in_codecpar);
+        if (ret < 0) {
+            av_strerror(ret,errors,1024);
+            av_log(NULL,AV_LOG_ERROR,"Failed to copy codec parameters: %d(%s)\n",ret,errors);
+            goto end;
+        }
+        out_stream->codecpar->codec_tag = 0;
+		// //Copy the settings of AVCodecContext
+		// ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
+		// if(ret < 0){
+		// 	av_strerror(ret,errors,1024);
+        //     av_log(NULL,AV_LOG_ERROR,"Failed to copy context from input to output stream codec context: %d(%s)\n",ret,errors);
+		// 	goto end;
+		// }
+		// out_stream->codec->codec_tag = 0;
+		// if(ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER) {
+        //     av_log(NULL,AV_LOG_DEBUG,"oformat->flags: %d\n",ofmt_ctx->oformat->flags);
+		// 	out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+		// }
 	}
 
-    av_log(NULL,AV_LOG_DEBUG,"input file nb_streams: %d\n",ifmt_ctx->nb_streams);
+    
 	//Output information------------------
 	av_dump_format(ofmt_ctx, 0, out_filename, 1);
 
-	//打开输出文件(Open output file)
+	//Open output file
 	if(!(ofmt->flags & AVFMT_NOFILE)) {
         av_log(NULL,AV_LOG_DEBUG,"ofmt->flags: %d\n",ofmt->flags);
 		ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
@@ -134,24 +147,22 @@ int main(int argc, char* argv[])
 		if(ret < 0)
 			break;
 		in_stream  = ifmt_ctx->streams[pkt.stream_index];
+		if (pkt.stream_index >= stream_mapping_size ||
+            stream_mapping[pkt.stream_index] < 0) {
+            av_packet_unref(&pkt);
+            continue;
+        }
+
+		pkt.stream_index = stream_mapping[pkt.stream_index];
 		out_stream = ofmt_ctx->streams[pkt.stream_index];
 
-		//转换PTS/DTS(Convert PTS/DTS)
+		//Convert PTS/DTS
 		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
 		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
 		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
 		pkt.pos = -1;
 
-		if(pkt.stream_index == 0){
-			 AVPacket fpkt = pkt; 
-			 int a = av_bitstream_filter_filter(vbsf,out_stream->codec,NULL,
-					                            &fpkt.data,&fpkt.size,pkt.data,pkt.size ,
-												pkt.flags & AV_PKT_FLAG_KEY); 
-			 pkt.data = fpkt.data;
-			 pkt.size = fpkt.size; 
-		}
-
-		//写入(Write)
+		//Write
 		ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
 		if(ret < 0){
 			av_strerror(ret,errors,1024);
@@ -159,11 +170,10 @@ int main(int argc, char* argv[])
 			break;
 		}
 	
-		av_log(NULL,AV_LOG_DEBUG,"Write %8d frames to output file,pkt.stream_index: %d\n",frame_index,pkt.stream_index);
+		//av_log(NULL,AV_LOG_DEBUG,"Write frames to output file,pkt.stream_index: %d\n",pkt.stream_index);
 		av_free_packet(&pkt);
-		frame_index++;
 	}
-	//写文件尾(Write file trailer)
+	//Write file trailer
 	av_write_trailer(ofmt_ctx);
 
 	//Output information------------------
@@ -175,6 +185,8 @@ end:
 	if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
 		avio_close(ofmt_ctx->pb);
 	avformat_free_context(ofmt_ctx);
+    
+	av_freep(&stream_mapping);
 
 	if (ret < 0 && ret != AVERROR_EOF) {
 		printf( "Error occurred.\n");
